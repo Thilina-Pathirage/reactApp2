88f77dc8d78c48f2506c24347e59ad52fb675b24
Thilina Pathirage <62783923+Thilina-Pathirage@users.noreply.github.com>
2021-09-25 18:07:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Test from './components/Test';
+import Sample from './components/Sample';
 
 function App() {
   return (
     <div className="App">
-      <Test/>
+      <Sample/>
     </div>
   );
 }
